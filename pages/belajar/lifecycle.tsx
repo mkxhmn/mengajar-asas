f5ed296876c83c1460ff7d1fec9a1262733458f2
Farid <fnikman@gmail.com>
2021-11-24 17:09:13 +0800

    feat: implement new layout

--- pages/belajar/lifecycle.tsx
+++ pages/belajar/lifecycle.tsx
@@ -1,6 +1,9 @@
-import { ChangeEvent, FunctionComponent, useState } from "react";
+import { ChangeEvent, Fragment, FunctionComponent, useState } from "react";
 import { ISpaceXResponse, useCapsules } from "../../src/hooks/useCapsules";
 import { GetServerSideProps } from "next";
+import { Header } from "../../src/components/Header";
+import { HeroBanner } from "../../src/components/HeroBanner";
+import { Container } from "../../src/components/Container";
 
 const Lifecycle: FunctionComponent<{ data: ISpaceXResponse[] }> = (props) => {
   // - [x] lifecycle
@@ -23,37 +26,36 @@ const Lifecycle: FunctionComponent<{ data: ISpaceXResponse[] }> = (props) => {
   );
 
   return (
-    <div className="container lg:min-w-1/2 mx-auto">
-      <h1>value kita: {capsule_id}</h1>
-      <input
-        className="border-black border"
-        id="contoh-dependency"
-        value={capsule_id}
-        onChange={handleInput}
-      />
-      {spaceXCapsulesData.map((capsule) => (
-        <div key={capsule.capsule_serial}>
-          <div>status: {capsule.status}</div>
-          <div>id: {capsule.capsule_id}</div>
-          <div>
-            missions:
-            {capsule.missions.map((mission) => (
-              <div key={mission.name + mission.flight}>
-                name: {mission.name}, flight: {mission.flight}
-              </div>
-            ))}
+    <Fragment>
+      <Header />
+      <HeroBanner />
+      <Container>
+        {spaceXCapsulesData.map((capsule) => (
+          <div key={capsule.capsule_serial}>
+            <div>status: {capsule.status}</div>
+            <div>id: {capsule.capsule_id}</div>
+            <div>
+              missions:
+              {capsule.missions.map((mission) => (
+                <div key={mission.name + mission.flight}>
+                  name: {mission.name}, flight: {mission.flight}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </Container>
+    </Fragment>
   );
 };
 
 //@ts-ignore
 export const getServerSideProps: GetServerSideProps = async () => {
-  const data = await fetch("https://api.spacexdata.com/v3/capsules")
+  const data: ISpaceXResponse[] = await fetch(
+    "https://api.spacexdata.com/v3/capsules"
+  )
     .then((response) => response.json())
-    .then((data: ISpaceXResponse[]) => data);
+    .then((data) => data);
 
   return {
     props: { data },
